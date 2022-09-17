774e7fff60f89a49fcb1da7eaacb50bbac63d786
rowlettron <rowlettron@yahoo.com>
2022-09-17 18:36:06 -0500

    Addition of 6 files

---
+++ RPEx1.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+window = tk.Tk()
+message = tk.Label(text="Python rocks!")
+message.pack()
+
+window.mainloop()
+
---
+++ RPGeometryManager1.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+window = tk.Tk()
+
+frame1 = tk.Frame(master=window, width=100, height=100, bg="red")
+frame1.pack(fill=tk.X)
+
+frame2 = tk.Frame(master=window, width=50, height=50, bg="yellow")
+frame2.pack(fill=tk.X)
+
+frame3 = tk.Frame(master=window, width=25, height=25, bg="blue")
+frame3.pack(fill=tk.X)
+
+
+window.mainloop()
+
